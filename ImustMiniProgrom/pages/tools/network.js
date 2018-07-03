592b31cb2212286bf7fd4963ebb70790f6dc7094
Zhangjunimust <Zhangjunimst@gmail.com>
2018-07-03 15:17:13 +0800

    change

--- ImustMiniProgrom/pages/tools/network.js
+++ ImustMiniProgrom/pages/tools/network.js
@@ -1,5 +1,6 @@
 //Pre  && Production
 const Network_Environment = "Pre";
+const Network_Header_Tokens = "Network_Header_Tokens"
 
 export function currentHost(){
   var host,cdn_host;
@@ -13,6 +14,52 @@ export function currentHost(){
   return { host: host, cdnHost: cdn_host}
 }
 
-export function test() {
-  console.log('333')
+export function fetchData(url, isUseCDN, params, method = "GET", header = {}, success, fail) {
+  //handle url
+  var requestUrl = isUseCDN ? currentHost().cdnHost + url : currentHost().host + url; 
+  if (url.includes("http://") || url.includes("https://")) {
+    requestUrl = url;
+  }
+  //handle header
+  var requestHeader = {};
+  requestHeader['content-type'] = 'application/json';
+  if (header){
+    for (var i in header){
+      requestHeader[i] = header[i]
+    }
+  }
+  //get user token
+  try {
+    var value = wx.getStorageSync(Network_Header_Tokens)
+    if (value) {
+      
+    }
+  } catch (e) {
+    
+  }
+  console.log('send request: ' + requestUrl)
+  wx.request({
+    url: requestUrl,
+    data: params,
+    header: requestHeader,
+    method: method,
+    success: function (res) {
+      if(success){
+        success(res)
+      }else{
+
+      }
+    },
+    fail: function (err) {
+      if(fail){
+        fail(err)
+      }else{
+
+      }
+    }
+  })
+
 }
+
+
+
---
+++ ImustMiniProgrom/services/constant.js
@@ -0,0 +1,1 @@
+const BASE_URL = 'https://pre.dealglobe.com';
